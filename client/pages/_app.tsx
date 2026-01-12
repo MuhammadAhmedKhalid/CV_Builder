76bf975dfe3ac4f322ff8620257cf13e85846c05
MuhammadAhmedKhalid <muhammadahmedkhalid4@gmail.com>
2026-01-12 16:49:45 +0500

    chore: update global font to Inter

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,24 +2,20 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import "@/styles/globals.css";
 import { IMAGES } from "@/lib/paths";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div className={inter.className}>
       <Head>
         <title>CV Builder</title>
-
-        {/* Tab icon / favicon */}
-        <link
-          rel="icon"
-          href={IMAGES.ICON}
-          type="image/png"
-        />
-
+        <link rel="icon" href={IMAGES.ICON} type="image/png" />
         <meta name="description" content="Build professional CVs easily" />
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
